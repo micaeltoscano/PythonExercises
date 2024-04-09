7a19a33726ef33dd3a3ea66d88c681bde2f555e4
Micael Toscano <cont.micaeltoscano@gmail.com>
2024-04-09 13:20:28 -0300

    Update funcoes.py

--- Threads/funcoes.py
+++ Threads/funcoes.py
@@ -1,44 +1,31 @@
-import math as mt
-
-def esquerda(lista):
-    meio = mt.floor(len(lista) / 2)
-    maior_esquerda = 0
-    while meio > -1:
-        n = lista[meio]
-      
-        if n > maior_esquerda:
-            maior_esquerda = n
-        meio -= 1
-    return maior_esquerda
-
-def direita(lista):
-    lista_direita = []
-    meio = (mt.floor(len(lista) / 2)) + 1
-    maior_direita = 0
-    while (meio < len(lista) - 1):
-        m = lista[meio]
-        lista_direita.append(m)
-        if m > maior_direita:
-            maior_direita = m
+def segunda_metade(lista):
+    lista_segunda_metade = []
+    meio = (len(lista)/4 + 1)
+    while (meio < len(lista) / 2):
+        lista_segunda_metade.append(lista[meio])
         meio += 1
-    return maior_direita
-
-def lista_esquerda(lista):
-    meio = int((len(lista) / 2))
-    lista_esquerda = []
-    while meio > -1:
-        n = lista[meio]
-        lista_esquerda.append(n)
-        meio -= 1
-    print((lista_esquerda))
-
-def lista_direita(lista):
-    meio = (mt.floor(len(lista) / 2)) + 1
-    lista_direita = []
-    while meio < (len(lista)):
-        m = lista[meio]
-        lista_direita.append(m)
+        #print("segunda metade funcionando")
+    return max(lista_segunda_metade)   
+ 
+def terceira_metade(lista):
+    lista_terceira_metade = []
+    meio = (len(lista)/2 + 1)
+    while (meio < (len(lista) / 2 + len(lista) / 4)):
+        lista_terceira_metade.append(lista[meio])
         meio += 1
-    print((lista_direita))
+        #print("terceira metade funcionando")
+    return max(lista_terceira_metade)    
+
+
+def quarta_metade(lista):
+    lista_quarta_metade = []
+    meio = (len(lista) / 2 + len(lista) / 4) + 1
+    while (meio < len(lista)):
+        lista_quarta_metade.append(lista[meio])
+        meio += 1
+        #print("terceira metade funcionando")
+    return max(lista_quarra_metade)    
+
+
 
 
