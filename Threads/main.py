87b7455dc04ac97a08c5ca55a343711154fe3d16
Micael Toscano <cont.micaeltoscano@gmail.com>
2024-04-09 16:40:38 -0300

    Update main.py

--- Threads/main.py
+++ Threads/main.py
@@ -3,20 +3,25 @@ import random as rd
 import threading
 import funcoes
 
-#Criação do vetor:
-lista = [rd.randint(0, 1000) for n in range(1000)]
+#Criação do vetor de 1 milhão de números:
+lista = [rd.randint(0, 999999) for n in range(1000000)]
 
 #Visualização das listas criadas:
+print(lista)
+funcoes.exibicao_lista(funcoes.primeiro, funcoes.segunda_metade, funcoes.terceira_metade, funcoes.quarta_metade, lista)
 
 #Criação da thread:
-thread1 = threading.Thread(target=funcoes.segunda_metade, args=(lista,)).start()
-thread2 = threading.Thread(target=funcoes.terceira_metade, args=(lista,)).start()
-thread3 = threading.Thread(target=funcoes.quarta_metade, args=(lista,)).start()
+thread1 = threading.Thread(target=funcoes.segunda_metade, args=(lista,))
+thread2 = threading.Thread(target=funcoes.terceira_metade, args=(lista,))
+thread3 = threading.Thread(target=funcoes.quarta_metade, args=(lista,))
 
-thread1.join()
-thread2.join()
-thread3.join()
+#Inicialização:
+funcoes.inicializacao(thread1,thread2,thread3)
 
-#chamando a def independente:
-funções.primeiro(lista)
+#Exibição dos resultados:
+print(f'''O maior número da primeira metade: {max(funcoes.primeiro(lista))} 
+O maior número da segunda metade: {max(funcoes.segunda_metade(lista))}  
+O maior número da terceira metade: {max(funcoes.terceira_metade(lista))} 
+O maior número da quarta metade: {max(funcoes.quarta_metade(lista))} ''')
+
 
